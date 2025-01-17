a194cf7de0b8bf2d206842acdd109f694a318913
aswanthup06 <aswanthup06@gmail.com>
2025-01-17 11:07:01 +0530

    banner

--- src/Pages/HomePage/Comp/Adbanner.jsx
+++ src/Pages/HomePage/Comp/Adbanner.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 export default function Adbanner() {
   return (
-    <div className='container mx-auto px-4 lg:px-24 h-44 overflow-hidden object-cover'>
-        
-       <img src="https://images.unsplash.com/photo-1734779206772-f21d663e96d5?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-        
-    </div>
+    <div className="container mx-auto px-4 lg:px-24 h-80 flex justify-center items-center overflow-hidden">
+    <img 
+      className="object-cover h-full w-full" 
+      src="https://www.lexusindia.co.in/content/dam/lexus-v3-blueprint/homepage/promotion-tile/hva/book-test-drive-desktop.jpg" 
+      alt="" 
+    />
+  </div>
   )
 }
